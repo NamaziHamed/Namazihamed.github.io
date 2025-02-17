c5d56cde5ba4727985ecee2ecc270a6e9eeebc5b
Hamed Namazi <H.namazi91@gmail.com>
2025-02-17 18:30:04 +0330

    timer component added - context added

---
+++ src/components/clock.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class Clock extends Component {
+
+    render() { 
+        return (
+            <h1>00 : 00</h1>
+        );
+    }
+}
+ 
+export default Clock;
