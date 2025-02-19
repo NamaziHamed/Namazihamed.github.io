ac56456479f0bbb672d120e5a58c659b3b580ac1
Hamed Namazi <H.namazi91@gmail.com>
2025-02-19 14:20:59 +0330

    timer running

--- src/App.jsx
+++ src/App.jsx
@@ -7,38 +7,70 @@ import InitiatorBtn from "./components/initiatorBtn";
 
 class App extends Component {
   state = {
-    pomodoroTimer: Duration.fromObject({ minutes: 25 }),
-    shortBreak: Duration.fromObject({ minutes: 5 }),
-    longBreak: Duration.fromObject({ minutes: 30 }),
+    pomodoroTimer: 25,
+    shortBreak: 5,
+    longBreak: 30,
     remainingTime: Duration.fromObject({ minutes: 25 }),
     pomodoroCount: 0,
-    currentStatus: "stop"
+    currentStatus: "stop",
   };
 
-  handleBtnClick = (event)=>{
-    const eventType = event.target.id
-    switch (eventType){
-      case "pomodoro":{ 
-        this.updateRemainingTime(this.state.pomodoroTimer)
-        break;
-      }
-      case "short-break":{ 
-        this.updateRemainingTime(this.state.shortBreak)
-        break;
-      }
-      case "long-break":{ 
-        this.updateRemainingTime(this.state.longBreak)
-        break;
-      }
-      case "start":{}
-      case "pause":{}
-      case "reset":{}
-    }
+  componentDidMount (){
+    this.applyBtnStyle()
   }
 
-  updateRemainingTime = (int)=>{
-    this.setState({remainingTime:int})
+  applyBtnStyle = ()=>{
+    const buttons = document.querySelectorAll("button")
+
+    buttons.forEach(btn=>{
+      btn.classList.add("btn-outline-dark")
+      btn.classList.add("mx-1")
+      btn.classList.add("my-3")
+    })
   }
+  handleBtnClick = (event) => {
+    const eventType = event.target.id;
+    switch (eventType) {
+      case "pomodoro": {
+        this.updateRemainingTime(this.state.pomodoroTimer);
+        break;
+      }
+      case "short-break": {
+        this.updateRemainingTime(this.state.shortBreak);
+        break;
+      }
+      case "long-break": {
+        this.updateRemainingTime(this.state.longBreak);
+        break;
+      }
+      case "start": {
+        this.interval = setInterval(this.reduceRemainingTime, 1000);
+        this.setState({currentStatus:"start"})
+        return
+      }
+      case "pause": {
+      }
+      case "reset": {
+      }
+    }
+  };
+
+  reduceRemainingTime = () => {
+    this.setState((prevState) => {
+      const newRemainingTime = prevState.remainingTime.minus({ seconds: 1 });
+      if (newRemainingTime.as("seconds") <= 0) {
+        clearInterval(this.interval);
+        return {
+          remainingTime: Duration.fromObject({ minutes: 0, seconds: 0 }),
+        };
+      }
+      return { remainingTime: newRemainingTime };
+    });
+  };
+
+  updateRemainingTime = (int) => {
+    this.setState({ remainingTime: Duration.fromObject({ minutes: int }) });
+  };
   render() {
     return (
       <TimerContext.Provider
@@ -48,13 +80,14 @@ class App extends Component {
           shortBreak: this.state.shortBreak,
           longBreak: this.state.longBreak,
           pomodoroCount: this.state.pomodoroCount,
-          handleBtnClick : this.handleBtnClick
+          currentStatus: this.state.currentStatus,
+          handleBtnClick: this.handleBtnClick,
         }}
       >
         <div className="container pomodoro-section">
           <IntervalControllers />
-          <Clock/>
-          <InitiatorBtn/>
+          <Clock />
+          <InitiatorBtn />
         </div>
       </TimerContext.Provider>
     );
--- src/components/initiatorBtn.jsx
+++ src/components/initiatorBtn.jsx
@@ -1,15 +1,66 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import timerContext from "../context/timerContext";
 
 class InitiatorBtn extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <div className="container text-center justify-content-between">
-                <button className="btn">Start</button>
-                <button className="btn hide">Reset</button>
-            </div>
-        );
+  static contextType = timerContext;
+
+  handleDisplay = () => {
+    if (this.context.currentStatus === "stop") {
+      return {
+        display: "none",
+      };
+    } else {
+      return {
+        display: "block",
+      };
     }
+  };
+
+  handleStart = () => {
+    if (this.context.currentStatus === "stop") {
+      return {
+        display: "block",
+      };
+    } else {
+      return {
+        display: "none",
+      };
+    }
+  };
+
+  render() {
+    return (
+      <div className="container text-center justify-content-between">
+        <div className="row text-center ">
+          <div className="col" style={this.handleStart()}>
+            <button
+              id="start"
+              onClick={this.context.handleBtnClick}
+              className="btn col-4"
+            >
+              Start
+            </button>
+          </div>
+          <div style={this.handleDisplay()} className="row">
+            <button
+              id="pause"
+              onClick={this.context.handleBtnClick}
+              className="btn col-4"
+            >
+              Pause
+            </button>
+            <button
+              id="reset"
+              onClick={this.context.handleBtnClick}
+              className="btn  col-4"
+            >
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default InitiatorBtn;
