a6ea81e2f9fe01263174db5a0d53c2302d8d7b66
Hamed Namazi <H.namazi91@gmail.com>
2025-03-04 14:56:41 +0330

    aligned app to the center of screen

--- src/components/mainPage.jsx
+++ src/components/mainPage.jsx
@@ -9,16 +9,32 @@ class MainPage extends Component {
           {/* Social Media Icons */}
           <div className="col-md-1 order-md-1 order-3">
             <div className="row justify-content-center align-items-center">
-              <a className="col-lg-12 col-sm-1 col-md-12 col-1" id="link-linkedin" href="">
+              <a
+                className="col-lg-12 col-sm-1 col-md-12 col-1"
+                id="link-linkedin"
+                href=""
+              >
                 <i id="link-linkedin" className="fa-brands fa-linkedin"></i>
               </a>
-              <a className="col-lg-12 col-sm-1 col-md-12 col-1" id="link-insta" href="">
+              <a
+                className="col-lg-12 col-sm-1 col-md-12 col-1"
+                id="link-insta"
+                href=""
+              >
                 <i id="link-insta" className="fa-brands fa-instagram"></i>
               </a>
-              <a className="col-lg-12 col-sm-1 col-md-12 col-1" id="link-x" href="">
+              <a
+                className="col-lg-12 col-sm-1 col-md-12 col-1"
+                id="link-x"
+                href=""
+              >
                 <i id="link-x" className="fa-brands fa-x-twitter"></i>
               </a>
-              <a className="col-lg-12 col-sm-1 col-md-12 col-1" id="link-github" href="">
+              <a
+                className="col-lg-12 col-sm-1 col-md-12 col-1"
+                id="link-github"
+                href=""
+              >
                 <i id="link-github" className="fa-brands fa-github"></i>
               </a>
             </div>
@@ -33,13 +49,16 @@ class MainPage extends Component {
               Bootstrap and React to create responsive and dynamic user
               interfaces.
             </p>
-            <button className="btn element" href="">
-              Learn More
-            </button>
+            <div className="text-sm-center text-md-center m-3">
+              {" "}
+              <button className="btn element " href="">
+                Learn More
+              </button>
+            </div>
           </div>
           {/* img div */}
           <div className="col-md-4 order-md-3 order-1 img-div p-2">
-            <img  src={picture} alt="my picture" />
+            <img src={picture} alt="my picture" />
           </div>
         </div>
       </div>
