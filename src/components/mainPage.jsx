03cbda2d5b4e6778d8b83e37f18aa521d6ad6491
Hamed Namazi <H.namazi91@gmail.com>
2025-03-04 13:45:49 +0330

    social icons section display style fixed

--- src/components/mainPage.jsx
+++ src/components/mainPage.jsx
@@ -7,37 +7,39 @@ class MainPage extends Component {
       <div className="container">
         <div className="row justify-content-center align-items-center">
           {/* Social Media Icons */}
-          <div className="col-md-1">
-            <div className="row">
-              <a className="icon-ref col-12" id="link-linkedin" href="">
+          <div className="col-md-1 order-md-1 order-3">
+            <div className="row justify-content-center align-items-center">
+              <a className="col-lg-12 col-sm-1 col-md-12 col-1" id="link-linkedin" href="">
                 <i id="link-linkedin" className="fa-brands fa-linkedin"></i>
               </a>
-              <a className="icon-ref col-12" id="link-insta" href="">
+              <a className="col-lg-12 col-sm-1 col-md-12 col-1" id="link-insta" href="">
                 <i id="link-insta" className="fa-brands fa-instagram"></i>
               </a>
-              <a className="icon-ref col-12" id="link-x" href="">
+              <a className="col-lg-12 col-sm-1 col-md-12 col-1" id="link-x" href="">
                 <i id="link-x" className="fa-brands fa-x-twitter"></i>
               </a>
-              <a className="icon-ref col-12" id="link-github" href="">
+              <a className="col-lg-12 col-sm-1 col-md-12 col-1" id="link-github" href="">
                 <i id="link-github" className="fa-brands fa-github"></i>
               </a>
             </div>
           </div>
           {/* Intro text section */}
-          <div className="col-md-7">
+          <div className="col-md-7 order-md-2 order-2">
             <h1>I'am Hamed Namazi</h1>
-            <h2>Web Developer</h2>
+            <h2>Frontend Engineer</h2>
             <p>
-              Specializing in frontend development. I'm proficient in HTML, CSS,
-              and JavaScript, and I have experience working with Bootstrap and
-              React to create responsive and dynamic user interfaces.
+              Specializing in Crawler Application development. I am proficient
+              in HTML, CSS, and JavaScript, and I have experience working with
+              Bootstrap and React to create responsive and dynamic user
+              interfaces.
             </p>
-       <button className="btn btn-primary" href="">Learn More</button>
-
+            <button className="btn element" href="">
+              Learn More
+            </button>
           </div>
           {/* img div */}
-          <div className="col-md-4">
-            <img className="rounded-circle" src={picture} alt="my picture" />
+          <div className="col-md-4 order-md-3 order-1 img-div p-2">
+            <img  src={picture} alt="my picture" />
           </div>
         </div>
       </div>
