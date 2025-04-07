001b12ecb63fba836e9d34f20bc70624a3015411
Hamed <h.namazi91@gmail.com>
2025-04-07 16:34:26 +0430

    color scheme changed, hero section in progress

--- src/components/Sections/hero.tsx
+++ src/components/Sections/hero.tsx
@@ -1,7 +1,31 @@
+import Button from "../shared/button";
+
 export default function Hero() {
   return (
-<section>
-    <h1 className="bg-primary text-5xl">Hi I'm</h1>
-</section>  
-);
+    <section
+      className="max-w-xl h-screen
+      p-2 sm:p-4 flex flex-col justify-center gap-5"
+    >
+      <p className="text-normal">HELLO, I'M</p>
+      <h1 className="color-primary underline">Hamed Namazi</h1>
+      <h3 className="text-normal">Creative and Innovative Web Developer</h3>
+      <p className="text-normal pr-10">
+        Creative and dynamic web developer with strong foundations in full-stack
+        development. Skilled in creating visually compelling and highly
+        functional web applications using HTML, CSS, JavaScript, React.js,
+        Angular, and Node.js.
+      </p>
+
+      <div className="flex justify-around max-w-sm">
+        <Button
+          text="Get In Touch"
+          className="bg-primary text-overlay"
+        />
+        <Button
+          text="View My Work"
+          className="bg-body color-primary border-blue-600 border-2"
+        />
+      </div>
+    </section>
+  );
 }
---
+++ src/components/shared/button.tsx
@@ -0,0 +1,10 @@
+interface ButtonProps{
+    text:string;
+    className?: string;
+    func?:()=>void;
+}
+export default function Button({text, className, func}:ButtonProps){
+    return <button onClick={func} className={`font-semibold
+        px-10 py-5 rounded-lg transition-all duration-300 
+        hover:shadow-lg hover:scale-105 ${className}`}>{text}</button>
+}
